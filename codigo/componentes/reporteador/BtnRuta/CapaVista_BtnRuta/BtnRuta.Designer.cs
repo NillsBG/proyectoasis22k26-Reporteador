ebd723167cb9b5d43f7604a420fc7f9dc56aae63
VelvethChavez <saraimejia1605@gmail.com>
2026-09-15 14:26:20 -0600

    Funcionamiento del BtnBusqueda de Gabriela Pinto

--- codigo/componentes/reporteador/BtnRuta/CapaVista_BtnRuta/BtnRuta.Designer.cs
+++ codigo/componentes/reporteador/BtnRuta/CapaVista_BtnRuta/BtnRuta.Designer.cs
@@ -52,6 +52,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.BtnRutaReporte);
             this.Name = "BtnRuta";
             this.Size = new System.Drawing.Size(56, 56);
