ad7c2591d03c1fbb14aeb6430d30998983b95671
Gabriela Pinto <gpintog1@miumg.edu.gt>
2026-09-18 14:12:56 -0600

    Agregar BtnBusqueda Reporteador

---
+++ codigo/componentes/reporteador/BtnBusqueda_Reporteador/CapaVista_BtnBusqueda_Reporteador/BtnBusqueda_Reporteador.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CapaVista_BtnBusqueda_Reporteador
+{
+    partial class BtnBusqueda_Reporteador
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BtnBusqueda_Reporteador));
+            this.btnAccionBusqueda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAccionBusqueda
+            // 
+            this.btnAccionBusqueda.Image = ((System.Drawing.Image)(resources.GetObject("btnAccionBusqueda.Image")));
+            this.btnAccionBusqueda.Location = new System.Drawing.Point(0, 0);
+            this.btnAccionBusqueda.Name = "btnAccionBusqueda";
+            this.btnAccionBusqueda.Size = new System.Drawing.Size(75, 69);
+            this.btnAccionBusqueda.TabIndex = 0;
+            this.btnAccionBusqueda.UseVisualStyleBackColor = true;
+            // 
+            // BtnBusqueda_Reporteador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAccionBusqueda);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "BtnBusqueda_Reporteador";
+            this.Size = new System.Drawing.Size(75, 69);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAccionBusqueda;
+    }
+}
