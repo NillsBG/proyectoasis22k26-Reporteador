ab25965f6d95175231b287fd3ec28e4fdaf744ff
Jose Xavier Bolaños Tenas <jbolanost@miumg.edu.gt>
2026-09-18 12:35:37 -0600

    Implementación de funcionalidad del botón 'Actualizar'.

---
+++ codigo/componentes/reporteador/BtnActualizar_Reporteador/CapaControlador_BtnActualizar_Reporteador/ClsControladorBtnActualizar.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using CapaModelo_BtnActualizar.Repositorios;
+
+namespace CapaControlador_BtnActualizar
+{
+    public class ClsControladorBtnActualizar
+    {
+        private ClsRepositorioBtnActualizar _RepositorioBtnActualizar;
+
+        public ClsControladorBtnActualizar()
+        {
+            _RepositorioBtnActualizar = new ClsRepositorioBtnActualizar();
+        }
+
+        public DataTable BtnActualizarFuncObtenerReportes()
+        {
+            return _RepositorioBtnActualizar.BtnActualizarFuncObtenerTodos();
+        }
+    }
+}
