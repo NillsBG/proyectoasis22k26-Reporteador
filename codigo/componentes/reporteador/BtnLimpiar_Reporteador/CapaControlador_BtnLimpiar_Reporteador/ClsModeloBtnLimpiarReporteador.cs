6d4dfbd4ff22fccef4216668f90bb90153b4e510
briandlcs <bdelacruzs@miumg.edu.gt>
2026-09-18 14:45:45 -0600

    BtnLimpiarReporteador-Estandarizado

---
+++ codigo/componentes/reporteador/BtnLimpiar_Reporteador/CapaControlador_BtnLimpiar_Reporteador/ClsModeloBtnLimpiarReporteador.cs
@@ -0,0 +1,34 @@
+﻿using CapaModelo_BtnLimpiar_Reporteador.RepositoriosBtnLimpiarReporteador;  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaControlador_BtnLimpiar_Reporteador
+{
+    public class ClsModeloBtnLimpiarReporteador
+    {
+        private readonly ClsRepositorioBtnLimpiarReporteador _Repositorio;
+
+        public ClsModeloBtnLimpiarReporteador()
+        {
+            _Repositorio = new ClsRepositorioBtnLimpiarReporteador();
+        }
+
+  
+        public bool ReporteadorFuncEjecutarLimpieza(out string Mensaje)
+        {
+            bool Resultado = _Repositorio.ReporteadorFuncLimpiarDatos(out string Error);
+
+            if (Resultado)
+            {
+                Mensaje = "Los campos fueron limpiados correctamente.";
+                return true;
+            }
+
+            Mensaje = Error;
+            return false;
+        }
+    }
+}
