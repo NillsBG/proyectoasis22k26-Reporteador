61adfaa0143b2816e73358845e3c1320bb2d82e8
NillsBG <nillsberducido@gmail.com>
2026-09-18 18:14:27 -0600

    Archivos para el boton eliminar

---
+++ codigo/componentes/reporteador/BtnEliminar_Reporteador/CapaControlador_BtnEliminar_Reporteador/ClsModeloBtnEliminarReporteador.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaModelo_BtnEliminar_Reporteador;
+
+
+namespace CapaControlador_BtnEliminar_Reporteador
+{
+    public class ClsModeloBtnEliminarReporteador
+    {
+ 
+        private readonly
+            ClsRepositorioBtnEliminarReporteador
+            _RepositorioBtnEliminarReporteador;
+
+
+        public ClsModeloBtnEliminarReporteador()
+        {
+            _RepositorioBtnEliminarReporteador =
+                new ClsRepositorioBtnEliminarReporteador();
+        }
+
+
+        public string ReporteadorMetDeshabilitar(
+            int numeroReporte)
+        {
+            try
+            {
+                if (numeroReporte <= 0)
+                {
+                    return
+                        "El numero del reporte no es valido.";
+                }
+
+                if (_RepositorioBtnEliminarReporteador
+                    .ReporteadorMetEstaDeshabilitado(
+                        numeroReporte))
+                {
+                    return
+                        "El reporte ya se encuentra " +
+                        "deshabilitado.";
+                }
+
+                _RepositorioBtnEliminarReporteador
+                    .ReporteadorMetDeshabilitar(
+                        numeroReporte);
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return
+                    "Ocurrio un error al deshabilitar el " +
+                    "reporte." + Environment.NewLine +
+                    ex.Message;
+            }
+        }
+
+
+        public bool ReporteadorMetEstaDeshabilitado(
+            int numeroReporte)
+        {
+            try
+            {
+                return _RepositorioBtnEliminarReporteador
+                    .ReporteadorMetEstaDeshabilitado(
+                        numeroReporte);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        public IEnumerable<int>
+            ReporteadorMetObtenerNumerosDeshabilitados()
+        {
+            try
+            {
+                return _RepositorioBtnEliminarReporteador
+                    .ReporteadorMetObtenerTodos();
+            }
+            catch (Exception)
+            {
+                return new List<int>();
+            }
+        }
+    }
+}
---
+++ codigo/componentes/reporteador/BtnEliminar_Reporteador/CapaModelo_BtnEliminar_Reporteador/ClsRepositorio.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace CapaModelo_BtnEliminar_Reporteador
+{
+    // Clase base: sabe DONDE esta guardado el estado
+    // de "deshabilitados" y COMO leerlo/escribirlo.
+    // No toca la base de datos. El estado se guarda en
+    // un archivo de texto plano junto al ejecutable:
+    // un numero de reporte por linea.
+
+    public abstract class ClsRepositorio
+    {
+ 
+        protected readonly string _RutaArchivo;
+
+        // Evita choques si dos operaciones intentan
+        // leer/escribir el archivo al mismo tiempo.
+        private static readonly object _Candado =
+            new object();
+
+
+        public ClsRepositorio()
+        {
+            _RutaArchivo =
+                ReporteadorMetRutaPorDefecto();
+        }
+
+        private string ReporteadorMetRutaPorDefecto()
+        {
+            string carpeta =
+                AppDomain.CurrentDomain.BaseDirectory;
+
+            return Path.Combine(
+                carpeta,
+                "ReportesDeshabilitados.txt");
+        }
+
+
+        protected List<int> ReporteadorMetLeerNumeros()
+        {
+            lock (_Candado)
+            {
+                if (!File.Exists(_RutaArchivo))
+                {
+                    return new List<int>();
+                }
+
+                List<int> lista = new List<int>();
+
+                foreach (string linea
+                         in File.ReadAllLines(
+                             _RutaArchivo))
+                {
+                    int numero;
+
+                    if (int.TryParse(
+                            linea.Trim(),
+                            out numero))
+                    {
+                        lista.Add(numero);
+                    }
+                }
+
+                return lista;
+            }
+        }
+
+        protected void ReporteadorMetEscribirNumeros(
+            IEnumerable<int> numeros)
+        {
+            lock (_Candado)
+            {
+                File.WriteAllLines(
+                    _RutaArchivo,
+                    numeros
+                        .OrderBy(n => n)
+                        .Select(n => n.ToString()));
+            }
+        }
+    }
+}
