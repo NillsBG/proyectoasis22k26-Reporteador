e8abeae6ba4361205649e57e5739841a089eed85
Jose Xavier Bolaños Tenas <jbolanost@miumg.edu.gt>
2026-09-22 14:27:24 -0600

    Implementando la funcionalidad del botón 'VER REPORTE'.

---
+++ codigo/componentes/reporteador/BtnVerReporte_Reporteador/CapaModelo_BtnVerReporte_Reporteador/Contratos/IRepositorioGenericoBtnVerReporte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_BtnVerReporte_Reporteador.Contratos
+{
+    public interface IRepositorioGenericoBtnVerReporte<Entity> where Entity : class
+    {
+        IEnumerable<Entity> GetAll();
+    }
+}
